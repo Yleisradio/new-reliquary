b1a68c04f346c0d9beb2f0e116564396541f7d5c
Juha Karemo <juha.karemo@reaktor.fi>
2014-09-19 11:45:22 +0300

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject new-reliquary "0.1.1"
+(defproject new-reliquary "0.1.2"
   :description "Clojure newrelic java api wrapper"
   :url "https://github.com/juhakaremo/new-reliquary"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.newrelic.agent.java/newrelic-api "3.5.1"]]
-  :profiles { :dev { :dependencies [[ring/ring-core "1.3.0-RC1"]
+                 [com.newrelic.agent.java/newrelic-api "3.10.0"]]
+  :profiles { :dev { :dependencies [[ring/ring-core "1.3.1"]
                                     [ring-mock "0.1.5"]
-                                    [org.clojure/tools.trace "0.7.6"]]}})
+                                    [org.clojure/tools.trace "0.7.8"]]}}
+  :plugins [[lein-ancient "0.5.5"]])
