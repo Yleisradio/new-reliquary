940a65947c7b71407719e39a6186f9379f589aab
Mikko Nylén <mnylen@reaktor.fi>
2015-01-23 09:49:41 +0200

    Change groupId to yleisradio

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject new-reliquary "0.1.3"
+(defproject yleisradio/new-reliquary "0.1.3"
   :description "Clojure newrelic java api wrapper"
   :url "https://github.com/Yleisradio/new-reliquary"
   :license {:name "Eclipse Public License"
