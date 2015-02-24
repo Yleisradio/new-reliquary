40c16119f34a6251d802b5acd53e5ef1465b275e
Juha Karemo <juha.karemo@reaktor.fi>
2015-02-24 14:04:22 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject yleisradio/new-reliquary "0.1.3"
+(defproject yleisradio/new-reliquary "0.1.4"
   :description "Clojure newrelic java api wrapper"
   :url "https://github.com/Yleisradio/new-reliquary"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.newrelic.agent.java/newrelic-api "3.10.0"]]
-  :profiles { :dev { :dependencies [[ring/ring-core "1.3.1"]
+                 [com.newrelic.agent.java/newrelic-api "3.13.0"]]
+  :profiles { :dev { :dependencies [[ring/ring-core "1.3.2"]
                                     [ring-mock "0.1.5"]
                                     [org.clojure/tools.trace "0.7.8"]]}}
   :plugins [[lein-ancient "0.5.5"]])
