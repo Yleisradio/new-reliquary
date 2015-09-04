7a3ca792fab4dd93094e9b639190f48ae374b3da
Matti Lankinen <matti.lankinen@reaktor.fi>
2015-09-04 10:10:02 +0300

    Prevent double error reporting of non-catched errors

--- src/new_reliquary/core.clj
+++ src/new_reliquary/core.clj
@@ -2,11 +2,8 @@
   (:import [com.newrelic.api.agent NewRelic Trace]))
 
 (definterface INewRelicTracable
-  (trace []))
-
-(deftype NewRelicTracer [callback] INewRelicTracable
-  (^{Trace {:dispatcher true}} trace [_]
-    (callback)))
+  (trace [])
+  (doTransaction []))
 
 (defn set-transaction-name [category name]
   (NewRelic/setTransactionName category name))
@@ -29,6 +26,19 @@
     (doseq [[key value] (seq custom-params)]
       (add-custom-parameter (str key) (str value)))
     (callback)))
+
+(deftype NewRelicTracer [callback] INewRelicTracable
+  (^{Trace {:dispatcher true}} trace [_]
+    (callback))
+  (doTransaction [this]
+    (try
+      (.trace this)
+      (catch Throwable e
+        ; .trace() method already reports the error due to @Trace annotation => we don't want that
+        ; NewRelic reports the error twice, thus ignore the outer ("global") transaction
+        ; TODO: how to resolve nested transactions case?
+        (ignore-transaction)
+        (throw e)))))
 
 (defn with-newrelic-transaction
   ([category transaction-name custom-params callback]
