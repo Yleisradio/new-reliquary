e4cb8276137ec7991c3639412180b64fbeca3258
Jaakko Suutarla <jaakko@suutarla.com>
2017-11-20 09:15:01 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/Yleisradio/new-reliquary"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.newrelic.agent.java/newrelic-api "3.17.0"]]
-  :profiles { :dev { :dependencies [[ring/ring-core "1.3.2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.newrelic.agent.java/newrelic-api "3.46.0"]]
+  :profiles { :dev { :dependencies [[ring/ring-core "1.6.3"]
                                     [ring-mock "0.1.5"]
-                                    [org.clojure/tools.trace "0.7.8"]]}}
-  :plugins [[lein-ancient "0.6.7"]]
+                                    [org.clojure/tools.trace "0.7.9"]]}}
+  :plugins [[lein-ancient "0.6.14"]]
   :signing {:gpg-key "C37817AC"})
