58c50f6e3309588dad7c564301dbd8fd112dbb5e
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2019-07-29 20:43:13 +0300

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.newrelic.agent.java/newrelic-api "4.0.0"]]
-  :profiles { :dev { :dependencies [[ring/ring-core "1.6.3"]
+                 [com.newrelic.agent.java/newrelic-api "5.3.0"]]
+  :profiles { :dev { :dependencies [[ring/ring-core "1.7.1"]
                                     [ring-mock "0.1.5"]
-                                    [org.clojure/tools.trace "0.7.9"]]}}
-  :plugins [[lein-ancient "0.6.14"]]
+                                    [org.clojure/tools.trace "0.7.10"]]}}
+  :plugins [[lein-ancient "0.6.15"]]
   :signing {:gpg-key "D9A17928"})
